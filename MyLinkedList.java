609b441b0c2126af8e4e58909f6a6059f60c0e3a
LucasDanielD <lucasdanielsantanarosa@gmail.com>
2026-05-24 18:16:50 -0300

    Add files via upload

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,95 +1,227 @@
 package Principal;
 
-public class MyLinkedList<T> implements MyList<T>{
-
+public class MyLinkedList<T> implements MyList<T> {
 
     private Node head;
-
     private int size;
 
-
     private class Node {
-        T data;       // O valor guardado
-        Node next;    // A referência para o próximo Node
+        T data;
+        Node next;
 
-        // Construtor do Node
         public Node(T data) {
             this.data = data;
-            this.next = null; // Por padrão, o próximo é nulo até que se conecte a algo
+            this.next = null;
         }
     }
 
-    // Construtor da Lista
     public MyLinkedList() {
         this.head = null;
         this.size = 0;
     }
 
 
+
     @Override
     public void addFirst(T element) {
-        // T
+        Node newNode = new Node(element);
+
+        newNode.next = head;
+
+        head = newNode;
+        size++;
+
     }
 
     @Override
     public void addLast(T element) {
-        // T
+        Node newNode = new Node(element);
+
+
+        if (head == null) {
+            head = newNode;
+        } else {
+
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
+        }
+        size++;
+
     }
 
     @Override
     public void insertAt(int index, T element) {
-        // T
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException("Índice fora dos limites");
+
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+
+        Node newNode = new Node(element);
+        Node current = head;
+
+
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+
+
+        newNode.next = current.next;
+
+        current.next = newNode;
+        size++;
+
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void addSorted(T element) {
-        // T
+
+        Node newNode = new Node(element);
+
+        if (head == null || ((Comparable<T>) head.data).compareTo(element) >= 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node current = head;
+
+            while (current.next != null && ((Comparable<T>) current.next.data).compareTo(element) < 0) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
     }
+
 
     @Override
     public T removeFirst() {
-        // T
-        return null;
+        if (head == null) return null;
+
+        T removedData = head.data;
+
+        head = head.next;
+        size--;
+        return removedData;
+
     }
 
     @Override
     public T removeLast() {
-        // T
-        return null;
+        if (head == null) return null;
+
+        if (head.next == null) {
+            return removeFirst();
+        }
+
+        Node current = head;
+
+        while (current.next.next != null) {
+            current = current.next;
+        }
+
+        T removedData = current.next.data;
+
+        current.next = null;
+        size--;
+        return removedData;
+
     }
 
     @Override
     public T removeAt(int index) {
-        // T
-        return null;
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Índice fora dos limites");
+
+        if (index == 0) return removeFirst();
+
+        Node current = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+
+        T removedData = current.next.data;
+
+        current.next = current.next.next;
+        size--;
+        return removedData;
     }
 
     @Override
     public boolean remove(T element) {
-        // T
+        if (head == null) return false;
+
+
+        if (head.data.equals(element)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node current = head;
+
+        while (current.next != null && !current.next.data.equals(element)) {
+            current = current.next;
+        }
+
+
+        if (current.next != null) {
+            current.next = current.next.next;
+            size--;
+            return true;
+        }
+
         return false;
     }
 
+
+
     @Override
     public void clear() {
-        // T
+
+        head = null;
+        size = 0;
     }
 
     @Override
     public int find(T element) {
-        // T
+        Node current = head;
+        int index = 0;
+
+        while (current != null) {
+            if (current.data.equals(element)) return index;
+            current = current.next;
+            index++;
+        }
         return -1;
     }
 
     @Override
     public T get(int index) {
-        // T
-        return null;
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Índice fora dos limites");
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
     }
 
     @Override
     public void set(int index, T element) {
-        // T
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Índice fora dos limites");
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.data = element;
     }
 
     @Override
@@ -97,5 +229,3 @@ public class MyLinkedList<T> implements MyList<T>{
         return this.size;
     }
 }
-
-
